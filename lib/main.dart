b09f842b87b937e7e0ceac733261f7ff125e6c55
kutucoding <anggifauzan2020@gmail.com>
2025-01-30 08:27:40 +0700

    memperbaiki image slider menjadi provider, memperbaiki detail loading

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:restaurant_app/data/api/api_services.dart';
 import 'package:restaurant_app/provider/detail/restaurant_detail_provider.dart';
+import 'package:restaurant_app/provider/home/image_slider_provider.dart';
 import 'package:restaurant_app/provider/home/restaurant_list_provider.dart';
 import 'package:restaurant_app/provider/home/restaurant_search_provider.dart';
 import 'package:restaurant_app/provider/main/index_nav_provider.dart';
@@ -25,6 +26,9 @@ void main() {
         create: (context) => RestaurantListProvider(
               context.read<ApiServices>(),
             )),
+    ChangeNotifierProvider(
+      create: (context) => ImageSliderProvider(),
+    ),
     ChangeNotifierProvider(
       create: (context) => RestaurantDetailProvider(
         context.read<ApiServices>(),
