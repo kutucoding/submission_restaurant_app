115b69792fcc4af4a18b6dcdf5a61306ef08f957
kutucoding <anggifauzan2020@gmail.com>
2025-02-02 08:32:26 +0700

    memperbaiki _textTheme

--- lib/provider/setting/theme_provider.dart
+++ lib/provider/setting/theme_provider.dart
@@ -1,9 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:restaurant_app/style/components/setting_theme.dart';
+import 'package:restaurant_app/style/theme/theme.dart';
 
 
 class ThemeProvider extends ChangeNotifier{
-  ThemeData _themeData = lightMode;
+  ThemeData _themeData = RestaurantTheme().lightMode;
 
   ThemeData get themeData => _themeData;
 
@@ -13,10 +13,10 @@ class ThemeProvider extends ChangeNotifier{
   }
 
   void toggleTheme() {
-    if (_themeData == lightMode){
-      themeData = darkMode;
+    if (_themeData == RestaurantTheme().lightMode){
+      themeData = RestaurantTheme().darkMode;
     } else {
-      themeData = lightMode;
+      themeData = RestaurantTheme().lightMode;
     }
   }
 }
--- lib/style/theme/theme.dart
+++ lib/style/theme/theme.dart
@@ -22,4 +22,24 @@ static TextTheme get _textTheme {
     );
   }
 
+  ThemeData lightMode = ThemeData(
+  brightness: Brightness.light,
+  textTheme: _textTheme,
+  colorScheme: ColorScheme.light(
+    surface: const Color(0xffFAD8B8),
+    primary:  Colors.grey.shade200,
+    secondary: Colors.grey.shade100,
+  )
+);
+
+ThemeData darkMode = ThemeData(
+  brightness: Brightness.dark,
+  textTheme: _textTheme,
+  colorScheme: ColorScheme.dark(
+    surface: Colors.grey.shade900,
+    primary: Colors.grey.shade800,
+    secondary: Colors.grey.shade700,
+  )
+);
+
 }
